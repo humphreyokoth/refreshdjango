8bd16cfdc7acaddff590f39179dfd195c63dfcda
humphreyokoth <humpasp@gmail.com>
2025-02-03 14:27:34 +0300

    Add banner field to Post model and update settings for media files

--- djangorefresh/djangorefresh/urls.py
+++ djangorefresh/djangorefresh/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -26,3 +28,4 @@ urlpatterns = [
     path('posts/',include('posts.urls')),    
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djangorefresh/posts/models.py
+++ djangorefresh/posts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
-
+    banner = models.ImageField(default="default.jpg", blank=True)
 
     def __str__(self):
         return self.title
