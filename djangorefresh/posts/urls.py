b0a55389d2e6c1e8fe41fc3abf6204a27783ab73
humphreyokoth <humpasp@gmail.com>
2025-02-03 13:41:26 +0300

    Add post detail view and update post list template with links

--- djangorefresh/posts/urls.py
+++ djangorefresh/posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-   
-    path('',views.posts_list),
+    path('',views.posts_list, name="posts"),
+    path('<slug:slug>',views.post_page, name="page"),
+
    
 ]
